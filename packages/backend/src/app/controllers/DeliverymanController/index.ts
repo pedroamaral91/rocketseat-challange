e202149b5a101a2a06bbf9ae4ebd7a4e944a7f17
Pedro Amaral <pedro.amaral@objective.com.br>
2020-02-05 11:47:56 -0300

    [BACKEND-2] Created logic to add deliveryman

---
+++ packages/backend/src/app/controllers/DeliverymanController/index.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import * as Yup from 'yup'
+import Deliveryman from '../../models/Deliveryman'
+
+jest.mock('jsonwebtoken', () => ({
+  verify: (): any => ({ admin: 1 })
+}))
+
+class DeliverymanController {
+  public async store (req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      avatar_id: Yup.number().required(),
+      email: Yup.string().email().required()
+    })
+
+    if (!await schema.isValid(req.body)) {
+      return res.status(400).json({ message: 'Validation fails' })
+    }
+
+    const deliveryman = await Deliveryman.create(req.body)
+
+    return res.status(201).json(deliveryman)
+  }
+}
+
+export default new DeliverymanController()
--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import SessionController from './app/controllers/SessionController'
 import RecipientController from './app/controllers/RecipientController'
+import Deliveryman from './app/controllers/DeliverymanController'
 
 import Auth from './app/middlewares/Auth'
 import IsAdmin from './app/middlewares/IsAdmin'
@@ -9,6 +10,7 @@ import IsAdmin from './app/middlewares/IsAdmin'
 const routes = Router()
 
 routes.post('/session', SessionController.store)
-
 routes.post('/recipient', [Auth, IsAdmin], RecipientController.store)
+routes.post('/deliveryman', [Auth, IsAdmin], Deliveryman.store)
+
 export default routes
